ce4d20808578af154b61605808ac6677dc811d14
Juan D. Diaz <inf.juandiaz@gmail.com>
2020-07-09 19:23:54 +0100

    Trump API added

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const bot = new App({
 //BOT WAS MENTIONED
 bot.event("app_mention", async ({context, event}) => {
 
-	const valid_commands = ['joke', 'jokes']
+	const valid_commands = ['joke', 'jokes', 'juan', 'trump', 'gokhan']
 
 	//Detect what is saved here
 	const args = event.text.split(" ")
@@ -28,25 +28,48 @@ bot.event("app_mention", async ({context, event}) => {
 	//THE COMMAND IS VALID 
 	if (valid_commands.includes(command)){
 
+		let response = 'No response'
+
+		//Joke required
 		if(command == 'joke' || command == 'jokes'){
 
 			//Scrape the page to find the joke
 			const wholepage = await axios.get('https://icanhazdadjoke.com/')
 			const string_page = wholepage.data
 			const crop_one = string_page.split(`<p class="subtitle">`)
-			const joke = crop_one[1].split(`</p>`)[0]
+			response = crop_one[1].split(`</p>`)[0]
 
-			//Try to responde the joke
-			try{
-				await bot.client.chat.postMessage({
-					token: context.botToken,
-					channel: event.channel,
-					text: `${joke}`
-				})
-			}catch(e){
-				console.log(`Error responding ${e}`)
-			}
+		//Juan required
+		}else if(command == 'juan'){
+			response = ':juanito::juanito::juanito:'
 
+		//Gokhan required
+		}else if(command == 'gokhan'){
+			response = ':gokhan::gokhan::gokhan:'
+		
+		//Trump required
+		}else if(command == 'trump'){
+			const trump = await axios.get('http://www.tronalddump.io/random/quote')
+
+			//Convert ISOString to readable date
+			const date = new Date(trump.data.appeared_at)
+			const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+			const finaldate = date.getDate() + " " + months[date.getUTCMonth()] + " " + date.getFullYear()
+			
+			const source = trump.data._embedded.source[0].url
+			response = trump.data.value + '\n\n' + "[" + finaldate + "] -> " + `<${source}|Source>`
+
+		}
+
+		//Try to responde the joke
+		try{
+			await bot.client.chat.postMessage({
+				token: context.botToken,
+				channel: event.channel,
+				text: `${response}`
+			})
+		}catch(e){
+			console.log(`Error responding ${e}`)
 		}
 
 	//THE COMMAND IS NOT VALID
